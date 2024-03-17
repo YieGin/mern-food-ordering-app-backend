8b7c392adc4c07cd03b12da03c38e48052ab284f
YieGin <pumasami7@gmail.com>
2024-03-17 18:40:17 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import mongoose from 'mongoose'
+import myUserRoute from './routes/myUserRoute'
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log('Connected to database!'));
+
+const app = express()
+const port = 7000
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/health', (req: Request, res: Response) => {
+  res.send({ message: 'health OK!' })
+})
+
+app.use('/api/my/user', myUserRoute)
+ 
+app.listen(port, () => {
+  console.log(`Your server is running on ${port}`);
+})
---
+++ src/routes/myUserRoute.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import MyUserController from "../controllers/MyUserController";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyUserRequest } from "../middleware/validation";
+
+const router = express.Router();
+
+// /api/my/user
+router.get('/', jwtCheck, jwtParse, MyUserController.getCurrentUser)
+router.post('/', jwtCheck, MyUserController.createCurrentUser)
+router.put('/', jwtParse, jwtCheck, validateMyUserRequest, MyUserController.updateCurrentUser)
+
+export default router
